b462959ef80109e13be41a701df7a6ce54eb26a4
xaneeshax <64504643+xaneeshax@users.noreply.github.com>
2021-03-18 16:09:55 -0700

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -10,9 +10,22 @@ void print_array(int array[], int size)
   std::cout << std::endl;
 }
 
+
+// const does not change the value at the memory address
+void print_array_const(const int data[], int size)
+{
+  for(int i = 0; i < size; i++)
+  {
+    data[i]++;
+    std::cout << data[i] << "\t";
+  }
+  std::cout << "\n";
+}
+
 int main()
 {
 
+  // Get suze of an array
   int guesses[] = {12, 23, 34, 45, 56};
   int size = sizeof(guesses) / sizeof(guesses[0]);
 
@@ -24,6 +37,7 @@ int main()
     std::cout << guesses[i] << "\t";
   }
   
+  // Nested array
   int grades[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
   for (int i = 0; i < 3; i++)
   {
